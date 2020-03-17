b535b001e4100321b063525d7ea6b527903659a5
Rubén <rubenalb90@gmail.com>
2020-03-17 21:23:21 +0100

    Updates

---
+++ precache-manifest.097319b6b593f70d1346ef927e255192.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ac1a41196a7575b57c6",
+    "url": "build/2.5ac1a411.js"
+  },
+  {
+    "revision": "db6a9118063be51ff6440eca68dbee11",
+    "url": "build/2.5ac1a411.js.LICENSE.txt"
+  },
+  {
+    "revision": "47e562ce73a2b423b95d",
+    "url": "build/bundle.47e562ce.js"
+  },
+  {
+    "revision": "5993f7b195c5cfbaf23c",
+    "url": "build/main.5993f7b1.js"
+  },
+  {
+    "revision": "6f2ba539773a61a0984aa1d0bc51f282",
+    "url": "index.html"
+  },
+  {
+    "revision": "5993f7b195c5cfbaf23c",
+    "url": "static/css/main.3729f750.chunk.css"
+  }
+]);
