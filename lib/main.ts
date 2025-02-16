2d5addbf67ffe218a6d192846b53c9f0a01b85a1
Rubén <rubenalb90@gmail.com>
2025-02-16 21:31:11 +0100

    Basic setup Vite lib build

---
+++ lib/main.ts
@@ -0,0 +1,3 @@
+export function usePulseText(text: string): string {
+    return `using react-pulse-text with ${text}!`
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
 import { defineConfig } from 'vite'
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url';
 import react from '@vitejs/plugin-react'
+import dts from 'vite-plugin-dts'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    dts({
+      tsconfigPath: resolve(__dirname, "tsconfig.lib.json"),
+    }),
+  ],
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'lib/main.ts'),
+      formats: ['es']
+    },
+    copyPublicDir: false,
+  }
 })
