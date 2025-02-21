81c3a02afe6079e70de3a7a0eec846699553d65a
Rubén <rubenalb90@gmail.com>
2025-02-21 01:05:32 +0100

    Delete _old directory

--- jest.config.ts
+++ jest.config.ts
@@ -6,7 +6,7 @@ const config: Config = {
     "^.+\\.tsx?$": "ts-jest", // Use ts-jest for TypeScript files
   },
   setupFilesAfterEnv: ["<rootDir>/jest.setup.ts"], // Setup file for testing utilities
-  testPathIgnorePatterns: ["/node_modules/", "/dist/", "/_old"], // Ignore these folders
+  testPathIgnorePatterns: ["/node_modules/", "/dist/"], // Ignore these folders
 };
 
 export default config;
