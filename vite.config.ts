f02a2ad4b6c2b4a8d7b97eab25d3f714596be025
Rubén <rubenalb90@gmail.com>
2025-02-22 10:33:50 +0100

    Set it up for publishing

--- vite.config.ts
+++ vite.config.ts
@@ -20,6 +20,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, "lib/main.ts"),
       formats: ["es"],
+      fileName: "main",
     },
     rollupOptions: {
       external: ["react", "react/jsx-runtime"],
