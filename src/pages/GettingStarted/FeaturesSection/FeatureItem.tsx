214ac2dfc008d1aacb3c7299d492825a3723a6da
Rubén <rubenalb90@gmail.com>
2025-02-27 00:06:44 +0100

    Animate feature descriptions

--- src/pages/GettingStarted/FeaturesSection/FeatureItem.tsx
+++ src/pages/GettingStarted/FeaturesSection/FeatureItem.tsx
@@ -1,14 +1,26 @@
-import { ReactNode } from "react";
 import CheckIcon from "../../../components/icons/CheckIcon.tsx";
+import usePulseText from "../../../../lib/hooks/usePulseText.ts";
 
 import styles from "./styles.module.css";
 
-export default function FeatureItem({ title, children }: { title: string; children: ReactNode }) {
+type FeatureItemProps = {
+  title: string;
+  position: number;
+  children: string;
+};
+
+export default function FeatureItem({ title, position, children }: FeatureItemProps) {
+  const { text: featureDescription } = usePulseText({
+    text: children,
+    duration: 500,
+    delay: position * 500,
+  });
+
   return (
     <li className={styles["feature-item"]}>
       <CheckIcon />{" "}
       <span>
-        <strong>{title}</strong>: {children}
+        <strong>{title}</strong>: {featureDescription}
       </span>
     </li>
   );
--- src/pages/GettingStarted/FeaturesSection/index.tsx
+++ src/pages/GettingStarted/FeaturesSection/index.tsx
@@ -5,25 +5,33 @@ export default function FeaturesSection() {
   return (
     <Section heading="Features">
       <ul>
-        <FeatureItem title="Text Animation">
+        <FeatureItem title="Text Animation" position={1}>
           The hook animates text by constructing it letter by letter over time, either forward or in reverse order.
         </FeatureItem>
-        <FeatureItem title="Erase Support">
+        <FeatureItem title="Erase Support" position={2}>
           It can display text character by character and erase it in a similar stepwise manner.
         </FeatureItem>
-        <FeatureItem title="Customizability">
+        <FeatureItem title="Customizability" position={3}>
           Allows configuration for animation duration, delay, number of iterations, and iteration delay.
         </FeatureItem>
-        <FeatureItem title="Callbacks">
+        <FeatureItem title="Callbacks" position={4}>
           Provides notifications (onStart, onChange, onEnd) at various stages of the animation lifecycle.
         </FeatureItem>
-        <FeatureItem title="Control Props">The animation can be toggled on/off with the active prop.</FeatureItem>
-        <FeatureItem title="Lightweight">Minimal package size, keeping your project lean and efficient.</FeatureItem>
-        <FeatureItem title="No dependencies">
+        <FeatureItem title="Control Props" position={5}>
+          The animation can be toggled on/off with the active prop.
+        </FeatureItem>
+        <FeatureItem title="Lightweight" position={6}>
+          Minimal package size, keeping your project lean and efficient.
+        </FeatureItem>
+        <FeatureItem title="No dependencies" position={7}>
           No additional libraries required, only specifying react as a peer dependency.
         </FeatureItem>
-        <FeatureItem title="TypeScript Support">Fully typed for rich development experience.</FeatureItem>
-        <FeatureItem title="Ease of use">Straightforward API for effortless integration.</FeatureItem>
+        <FeatureItem title="TypeScript Support" position={8}>
+          Fully typed for rich development experience.
+        </FeatureItem>
+        <FeatureItem title="Ease of use" position={9}>
+          Straightforward API for effortless integration.
+        </FeatureItem>
       </ul>
     </Section>
   );
