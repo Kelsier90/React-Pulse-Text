447184db25fdffeae5889857aa99da349c4a9542
Rubén <rubenalb90@gmail.com>
2025-03-13 21:59:13 +0100

    Add favicon and meta description

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./global.css";
 import App from "./App.tsx";
+
+import "./global.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
